3f061ec8cb2b982ad772648675b2b3fde16eba5e
Mike Tran <ngocmike238@gmail.com>
2020-08-06 18:25:19 -0400

    Production Build

---
+++ build/precache-manifest.11a0b194c758954336e8f4279f7dd970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae73ee9612602005251aec6fce6fe77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "372b56f3ef936607d52a",
+    "url": "/static/css/main.23063e55.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372b56f3ef936607d52a",
+    "url": "/static/js/main.09537618.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
